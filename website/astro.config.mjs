bd95c879347e0f92c84a62cbcbb9b1abe3bd7158
khoparzi <abhinayk@gmail.com>
2023-01-19 15:18:34 +0530

    another try at patternuary swatch

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -30,8 +30,8 @@ export default defineConfig({
     mdx(options),
     tailwind(),
   ],
-  site: `https://strudel.tidalcycles.org`,
-  base: '/',
+  site: `https://khoparzi.github.io`,
+  base: '/strudel',
 });
 
 /*
